bcff5817757725e2d6907aff9ce2b06fa92b57be
Diomede <diomede@mikamai.com>
2018-03-27 12:03:53 +0100

    solution step1

--- todo/models.py
+++ todo/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Project(models.Model):
   name = models.CharField(max_length=200)
 
+  def __str__(self):
+    return self.name
+
 class Todo(models.Model):
   project = models.ForeignKey(Project, on_delete=models.CASCADE)
   title = models.CharField(max_length=200)
