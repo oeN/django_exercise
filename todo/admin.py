cfcc04d8dba59ecf0bb170bf8556c36592042b59
Diomede <diomede@mikamai.com>
2018-03-27 18:40:18 +0100

    update solution

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Todo
 
-admin.site.register(Project)
+class TodoInline(admin.TabularInline):
+  model = Todo
+  extra = 3
+
+class ProjectAdmin(admin.ModelAdmin):
+  inlines = [TodoInline]
+
+admin.site.register(Project, ProjectAdmin)
+# admin.site.register(Project)
